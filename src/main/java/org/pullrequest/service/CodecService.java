68bfb254a94a0db88e1076bd891a663aff5d3b0e
Brian Clozel <brian.clozel@gmail.com>
2011-07-15 14:32:35 +0200

    Add CodecService for shorturls handling

---
+++ src/main/java/org/pullrequest/service/CodecService.java
@@ -0,0 +1,78 @@
+package org.pullrequest.service;
+
+/**
+ * 
+ * Codec Service that encodes/decodes decimal numbers from/to a 64 base.
+ * A few examples:
+ * <li> 1 => "0"</li>
+ * <li> 11 => "a"</li>
+ * <li> 63 => "="</li>
+ * <li> 64 => "10"</li>
+ * 
+ */
+public class CodecService {
+
+    public static final String codeset = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-=";
+    public static final long base = 64;
+    public static final long MAX_LENGTH = 6;
+    public static final long MAX_UID = Math.round(Math.pow(base,MAX_LENGTH));
+
+    /**
+     * Pick a random number in the following interval:
+     * [1,MAX_UID-1]
+     * @return a random uid
+     */
+    public static long generateUID() {
+        
+        return (long)(Math.random()*MAX_UID-2)+1;
+    }
+
+    /**
+     * Encode a number in another space using the given chars codeset
+     * @param uid number to be encoded
+     * @return encoded string
+     * @see CodecService.codeset
+     */
+    public static String encode(long uid) {
+
+        String hash = "";
+
+        while (uid > 0) {
+
+            int idx = (int) (uid % base);
+
+            hash = codeset.charAt(idx) + hash;
+            uid = (long)Math.floor(uid / base);
+        }
+
+        return hash;
+
+    }
+
+    /**
+     * Decode a string  from the given codeset to its long decimal value
+     * @param hash string to be decoded
+     * @return decimal value
+     * @see CodecService.codeset
+     */
+    public static long decode(String hash) {
+
+        long uid = 0;
+        int idx = 0;
+        int length = hash.toCharArray().length;
+
+        for (int aChar : hash.toCharArray()) {
+
+            int n = codeset.indexOf(aChar);
+            if (n == -1) {
+                return 0;
+            }
+
+            uid += n * Math.pow(base, length - idx - 1);
+
+            idx++;
+        }
+
+        return uid;
+    }
+}
---
+++ src/test/java/org/pullrequest/test/CodecServiceTest.java
@@ -0,0 +1,79 @@
+package org.pullrequest.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.pullrequest.service.CodecService;
+
+public class CodecServiceTest {
+    
+    @Test
+    public void testEncode() {
+
+        long uid = 1;
+        String hash = CodecService.encode(uid);
+        Assert.assertEquals("1", hash);
+        
+        uid = 63;
+        hash = CodecService.encode(uid);
+        Assert.assertEquals("=", hash);
+        
+        uid = 64;
+        hash = CodecService.encode(uid);
+        Assert.assertEquals("10", hash);
+        
+        uid = CodecService.MAX_UID-1;
+        hash = CodecService.encode(uid);
+        Assert.assertEquals("======", hash);
+    }
+    
+        @Test
+    public void testDecode() {
+
+        String hash = "1";
+        long uid = 1;
+        uid = CodecService.decode(hash);
+        Assert.assertEquals(1, uid);
+        
+        hash = "=";
+        uid = CodecService.decode(hash);
+        Assert.assertEquals(63, uid);
+ 
+        hash = "10";
+        uid = CodecService.decode(hash);
+        Assert.assertEquals(64, uid);
+        
+        hash = "======";
+        uid = CodecService.decode(hash);
+        Assert.assertEquals(CodecService.MAX_UID-1, uid);
+        
+    }
+    
+    @Test
+    public void testRandomUID() {
+
+        long firstuid = CodecService.generateUID();
+        long seconduid = CodecService.generateUID();
+        
+        Assert.assertTrue("generator should create random uids", firstuid != seconduid);
+    }
+    
+    @Test
+    public void testBijective() {
+
+        long uid = CodecService.generateUID();
+
+        String hash = CodecService.encode(uid);
+        
+        long decodedUid = CodecService.decode(hash);
+        
+        Assert.assertTrue("encoded and decoded UID should be the same", uid == decodedUid);
+    }
+
+    @Test
+    public void testValidUID() {
+
+        long sampleUID = CodecService.generateUID();
+        Assert.assertTrue("UID should be < "+CodecService.MAX_UID, sampleUID < CodecService.MAX_UID);
+        Assert.assertTrue("UID should be > 0", sampleUID > 0);
+    }
+}
