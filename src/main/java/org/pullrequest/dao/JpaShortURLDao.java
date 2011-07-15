96af4fe3e80986e57feb6166edc9ccaeec59c04b
Brian Clozel <brian.clozel@gmail.com>
2011-07-15 14:34:36 +0200

    Add new Dao methods for accessing ShortURLs

--- src/main/java/org/pullrequest/dao/JpaShortURLDao.java
+++ src/main/java/org/pullrequest/dao/JpaShortURLDao.java
@@ -1,12 +1,29 @@
 package org.pullrequest.dao;
 
 import javax.inject.Named;
+import javax.persistence.Query;
 
 import org.resthub.core.dao.GenericJpaDao;
-import org.pullrequest.dao.ShortURLDao;
 import org.pullrequest.model.ShortURL;
 
 @Named("shortURLDao")
 public class JpaShortURLDao extends GenericJpaDao<ShortURL, Long> implements ShortURLDao {
 
+    @Override
+    public ShortURL findByShortKey(String shortKey) {
+
+        String queryString = "from " + this.getDomainClass().getName() + " where shortKey = :shortKey";
+        Query q = this.getEntityManager().createQuery(queryString);
+        q.setParameter("shortKey", shortKey);
+        return (ShortURL) q.getSingleResult();
+    }
+
+    @Override
+    public boolean existsWithShortKey(String shortKey) {
+        String queryString = "from " + this.getDomainClass().getName() + " where shortKey = :shortKey";
+        Query q = this.getEntityManager().createQuery(queryString);
+        q.setParameter("shortKey", shortKey);
+        return q.getResultList().size()>0;
+    }
 }
+
--- src/main/java/org/pullrequest/dao/ShortURLDao.java
+++ src/main/java/org/pullrequest/dao/ShortURLDao.java
@@ -3,6 +3,9 @@ package org.pullrequest.dao;
 import org.resthub.core.dao.GenericDao;
 import org.pullrequest.model.ShortURL;
 
-public interface ShortURLDao extends GenericDao<ShortURL, Long>  {
+public interface ShortURLDao extends GenericDao<ShortURL, Long> {
 
+    public ShortURL findByShortKey(String shortKey);
+
+    public boolean existsWithShortKey(String shortKey);
 }
