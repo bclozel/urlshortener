a4bc968a8948aa6025bcfe83ccbb34182aa869fb
Brian Clozel <brian.clozel@gmail.com>
2011-08-03 16:32:20 +0200

    add crawlservlet and rewrite routes for AJAX crawl experiment

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -1,12 +1,12 @@
 define(['lib/resthub', 'shorturl/create.js', 'shorturl/infos.js', 'shorturl/list.js'], function() {
         
     // Define routes
-    $.route('#', function() {
+    $.route('#!', function() {
         $('#form').create_shorturl().show();
         $('#content').html('<span>Loading...</span>').list_shorturl();
     });
             
-    $.route('#/:shortkey', function(params) {
+    $.route('#!/:shortkey', function(params) {
         $('#form').hide().html('');
         $('#content').infos_shorturl({
             shortkey:params.shortkey
